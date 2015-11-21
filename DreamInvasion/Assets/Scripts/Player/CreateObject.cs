34af649f21b77bce73954d820f793536668fa1ef
stephdim <stephane.dimiceli@gmail.com>
2015-11-21 03:26:09 +0100

    Spawn square

---
+++ DreamInvasion/Assets/Scripts/Player/CreateObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateObject : MonoBehaviour {
+
+
+    Dictionary<ObjectType, Queue<Spawnable>> availableObj;
+    int[] currentObjs; // 0 : current, 1 : next
+
+    void Start() {
+        currentObjs = new int[2];
+        for (int i = 0; i < currentObjs.Length; ++i) {
+            currentObjs[i] = Random.Range(1, 101);
+        }
+	}
+	
+	void Update() {
+	    if (Input.GetButtonDown("Fire1")) {
+            Create();
+        }
+	}
+
+    void Create() {
+        if (currentObjs[0] <= 25) {
+            GameObject go = Instantiate(Resources.Load("Prefabs/Square"), transform.position, Quaternion.identity) as GameObject;
+        } else {
+            Debug.Log(currentObjs[0]);
+        }
+        currentObjs[0] = currentObjs[1];
+        currentObjs[1] = Random.Range(1, 101);
+    }
+}
---
+++ DreamInvasion/Assets/Scripts/Player/MoveCursor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dx = Input.GetAxis("Horizontal");
+        float dy = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(dx) >= 0.1f) {
+            transform.position += 0.5f * dx * Vector3.right;
+        }
+        if (Mathf.Abs(dy) >= 0.1f) {
+            transform.position += 0.5f * dy * Vector3.up;
+        }
+	}
+}
