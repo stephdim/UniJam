762da5317b464f302ff8d7acdad9c6f4b86be062
KhoubeibL <laouini7@hotmail.com>
2015-11-21 21:09:17 +0100

    Lifebar

---
+++ DreamInvasion/Assets/Scripts/Player/LockRot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockRot : MonoBehaviour {
+
+    bool test = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(transform.parent.localScale.x < 0) {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+	}
+}
