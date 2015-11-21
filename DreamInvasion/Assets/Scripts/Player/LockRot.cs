e1f053e41c13df786bd76ae7c3a183c1da26a7cd
KhoubeibL <laouini7@hotmail.com>
2015-11-21 22:08:29 +0100

    La barre de vie ne pivote pas

--- DreamInvasion/Assets/Scripts/Player/LockRot.cs
+++ DreamInvasion/Assets/Scripts/Player/LockRot.cs
@@ -13,10 +13,16 @@ public class LockRot : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if(transform.parent.localScale.x < 0) {
+        if(transform.parent.localScale.x < 0 && transform.localScale.x >0 ) {
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
         }
-	}
+
+        if (transform.parent.localScale.x > 0 && transform.localScale.x < 0) {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
 }
