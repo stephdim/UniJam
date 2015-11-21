b8b632c7b45f70bcb087c6a1f03cf0c3efbe3e92
KhoubeibL <laouini7@hotmail.com>
2015-11-21 10:23:11 +0100

    added scipts character, health, spike do damage

--- DreamInvasion/Assets/Scripts/Spawnable/Spike.cs
+++ DreamInvasion/Assets/Scripts/Spawnable/Spike.cs
@@ -7,8 +7,11 @@ public class Spike : MonoBehaviour {
     private float cooldown;
     private float timer;
     private bool isOut;
-	// Use this for initialization
-	void Start () {
+
+    [SerializeField]
+    float damage;
+    // Use this for initialization
+    void Start () {
         timer = cooldown;
         isOut = false;
 	}
@@ -30,6 +33,15 @@ public class Spike : MonoBehaviour {
         } else {
             transform.position -= Vector3.up;
             isOut = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.CompareTag("Player")) {
+            Debug.Log("hit" + "Player");
+
+            other.gameObject.GetComponent<Health>().takeDamage(this.damage);
         }
     }
 }
