d287c1da81e7db6889dfd93100abd205ea1f2caf
stephdim <stephane.dimiceli@gmail.com>
2015-11-22 10:16:52 +0100

    Last version

---
+++ DreamInvasion/Assets/Scripts/Spawnable/Fire.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+
+    Animator anim;
+    [SerializeField]
+    float damage;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.transform.CompareTag("Player")) {
+            Debug.Log("hit" + "Player");
+
+            other.gameObject.GetComponent<Health>().TakeDamage(this.damage);
+        }
+    }
+}
