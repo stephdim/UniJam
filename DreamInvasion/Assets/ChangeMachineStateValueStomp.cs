1b6a9526ae04187ba517e4d1f7d3e459c4dd205a
KhoubeibL <laouini7@hotmail.com>
2015-11-22 10:22:34 +0100

    presque la fin, 10h22

---
+++ DreamInvasion/Assets/ChangeMachineStateValueStomp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMachineStateValueStomp : MonoBehaviour {
+
+    private Animator m_Anim;
+
+    // Use this for initialization
+    void Start () {
+
+        m_Anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Alpha0)) {
+            m_Anim.SetInteger("stompState",0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            m_Anim.SetInteger("stompState", -1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            m_Anim.SetInteger("stompState", 1);
+
+        }
+
+    }
+}
