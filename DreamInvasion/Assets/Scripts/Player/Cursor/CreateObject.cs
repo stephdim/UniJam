6d257514aaa73521197dcd39aaf6b1cd65571f75
stephdim <stephane.dimiceli@gmail.com>
2015-11-22 11:17:24 +0100

    Spawn of objects works

--- DreamInvasion/Assets/Scripts/Player/Cursor/CreateObject.cs
+++ DreamInvasion/Assets/Scripts/Player/Cursor/CreateObject.cs
@@ -4,118 +4,104 @@ using System.Collections.Generic;
 
 public class CreateObject : MonoBehaviour {
 
-    
- //   int[] currentRand; // 0 : current, 1 : next
- //   GameObject[] currentGos;
- //   int current;
- //   DetectPlateform detectPlateform;
- //   GameObject pool;
- //   GameObject boss;
- //   public bool isOk;
- //   [SerializeField]
- //   float cooldown;
- //   float timer;
- //   bool canPut;
- //   Fire[] fire;
- //   Square[] square;
- //   Collider2D[] collider;
+    Vector2 posCurrent;
+    Vector2 posNext;
+    int nbObjs;
+    public bool isOk;
+    bool canPut;
+    GameObject[] objs;
+    DetectPlateform detectPlateform;
+    GameObject pool;
+    GameObject boss;
+    [SerializeField]
+    float cooldown;
+    float timer;
 
- //   void Start() {
- //       currentRand = new int[2];
- //       currentGos = new GameObject[2];
- //       collider = new Collider2D[2];
- //       fire = new Fire[2];
- //       square = new Square[2];
- //       pool = GameObject.FindGameObjectWithTag("Pool");
- //       boss = GameObject.FindGameObjectWithTag("Boss");
- //       //for (int i = 0; i < currentRand.Length; ++i) {
- //       //    currentRand[i] = Random.Range(1, 101);
- //       //}
- //       currentRand[0] = Random.Range(1, 101);
- //       Create();
- //       Create();
- //       detectPlateform = transform.parent.GetComponentInChildren<DetectPlateform>();
- //       isOk = true;
- //       timer = cooldown;
- //       canPut = true;
- //       current = 0;
-	//}
-	
-	//void Update() {
-	//    if (Input.GetButtonDown("Fire" + transform.parent.GetComponentInChildren<Stats>().id) && isOk && canPut) {
- //           PlaceObj();
- //           canPut = false;
- //       } else if (!canPut) {
- //           if (timer >= 0) {
- //               timer -= Time.deltaTime;
- //           } else {
- //               canPut = true;
- //               timer = cooldown;
- //           }
- //       }
-	//}
+    void Start() {
+        posCurrent = new Vector2(.15f, .5f);
+        posNext = new Vector2(1.93f, -.13f);
+        nbObjs = 0;
+        pool = GameObject.FindGameObjectWithTag("Pool");
+        boss = GameObject.FindGameObjectWithTag("Boss");
+        detectPlateform = transform.parent.GetComponentInChildren<DetectPlateform>();
+        objs = new GameObject[2];
+        isOk = true;
+        canPut = true;
+        timer = cooldown;
+    }
 
- //   void PlaceObj() {
- //       float y = -10;
- //       if (fire[(current + 1) % 2] != null) {
- //           fire[(current + 1) % 2].enabled = true;
- //       }
- //       for (int i = 0; i < detectPlateform.Plateforms.Count; ++i) {
- //           if (detectPlateform.Plateforms[i].transform.position.y <= transform.position.y && detectPlateform.Plateforms[i].transform.position.y > y) {
- //               y = detectPlateform.Plateforms[i].transform.position.y;
- //           }
- //       }
- //       if (y == -10) return;
+    void Update() {
+        if (nbObjs < 2) {
+            Create();
+            nbObjs++;
+        }
+        if (Input.GetButtonDown("Fire" + transform.parent.GetComponentInChildren<Stats>().id) && isOk && canPut) {
+            SetPos();
+            nbObjs--;
+        } else if (!canPut) {
+            if (timer >= 0) {
+                timer -= Time.deltaTime;
+            } else {
+                canPut = true;
+                timer = cooldown;
+            }
+        }
+    }
 
- //       currentGos[(current+1)%2].transform.SetParent(null);
- //       if (square[(current + 1) % 2] != null) {
- //           square[(current + 1) % 2].yMin = y;
- //           square[(current + 1) % 2].enabled = true;
- //       }
- //       currentGos[current].transform.position = currentGos[(current + 1) % 2].transform.position;
- //       if (fire[(current + 1) % 2] != null) {
- //           currentGos[(current + 1) % 2].transform.position = new Vector2(transform.position.x, y);
- //       } else {
- //           currentGos[(current + 1) % 2].transform.position = transform.position;
- //       }
- //       currentGos[(current + 1) % 2].transform.SetParent(pool.transform);
- //       currentGos[(current + 1) % 2] = currentGos[current];
- //       Create();
- //   }
+        void Create() {
+        int rand = Random.Range(1, 101);
+        GameObject go = new GameObject();
+        if (rand <= 50) {
+            go = Instantiate(Resources.Load("Prefabs/Fire")) as GameObject;
+        } else if (rand <= 100) {
+            go = Instantiate(Resources.Load("Prefabs/Square")) as GameObject;
+        }
 
- //   void Create() {
- //       //Vector3 pos;
- //       //if (current == 0) {
- //       //    pos = new Vector2(.15f, .5f);
- //       //} else {
- //       //    pos = new Vector2(1.93f, -.13f);
- //       //}
- //       //if (currentRand[0] <= 50) {
- //       //    GameObject go = Instantiate(Resources.Load("Prefabs/Square")) as GameObject;
- //       //    go.transform.SetParent(boss.transform);
- //       //    go.transform.localPosition = pos;
- //       //    go.transform.localScale = .5f * go.transform.localScale;
- //       //    currentGos[current] = go;
- //       //    square[current] = currentGos[current].GetComponent<Square>();
- //       //    square[current].enabled = false;
- //       //    collider[current] = currentGos[current].GetComponent<Collider2D>();
- //       //    collider[current].enabled = false;
- //       //} else if (currentRand[0] <= 100) {
- //       //    GameObject go = Instantiate(Resources.Load("Prefabs/Spike")) as GameObject;
- //       //    currentGos[current] = go;
- //       //    go.transform.SetParent(boss.transform);
- //       //    go.transform.localPosition = pos;
- //       //    go.transform.localScale = .5f * go.transform.localScale;
- //       //    fire[current] = currentGos[current].GetComponent<Fire>();
- //       //    collider[current] = currentGos[current].GetComponent<Collider2D>();
- //       //    collider[current].enabled = false;
- //       //    fire[current].enabled = false;
- //       //} else {
- //       //    Debug.Log(currentRand[0]);
- //       //}
- //       //currentRand[0] = currentRand[1];
- //       //currentRand[1] = Random.Range(1, 101);
- //       //current = (current + 1) % 2;
+        go.transform.SetParent(boss.transform);
+        go.transform.localScale = .5f * go.transform.localScale;
+        if (nbObjs == 0) {
+            go.transform.localPosition = posCurrent;
+            objs[0] = go;
+        } else {
+            go.transform.localPosition = posNext;
+            objs[1] = go;
+        }
+        for (int i = 0; i <= nbObjs; ++i) {
+            Debug.Log("obj : " + objs[i]);
+            objs[i].GetComponent<BoxCollider2D>().enabled = false;
+            if (objs[i].GetComponent<Fire>() != null) {
+                objs[i].GetComponent<Fire>().enabled = false;
+            } else {
+                objs[i].GetComponent<Square>().enabled = false;
+            }
+        }
+    }
 
- //   }
+    void SetPos() {
+        objs[0].transform.SetParent(null);
+
+        float y = -10;
+
+        for (int i = 0; i < detectPlateform.Plateforms.Count; ++i) {
+            if (detectPlateform.Plateforms[i].transform.position.y <= transform.position.y && detectPlateform.Plateforms[i].transform.position.y > y) {
+                y = detectPlateform.Plateforms[i].transform.position.y;
+            }
+        }
+        if (y == -10) return;
+
+        if (objs[0].GetComponent<Fire>() != null) {
+            objs[0].transform.position = new Vector2(transform.position.x, y + 0.33f);
+            objs[0].GetComponent<BoxCollider2D>().enabled = true;
+            objs[0].GetComponent<Fire>().enabled = true;
+        } else {
+            objs[0].transform.position = transform.position;
+            objs[0].GetComponent<BoxCollider2D>().enabled = true;
+            objs[0].GetComponent<Square>().enabled = true;
+            objs[0].GetComponent<Square>().yMin = y + 0.65f;
+        }
+        objs[0].transform.SetParent(pool.transform);
+        objs[0] = objs[1];
+        objs[0].transform.localPosition = posCurrent;
+        objs[1] = null;
+    }
 }
